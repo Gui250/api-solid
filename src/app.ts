02d3a73e33c0e3bff771e84cd9b1b46e30eedf77
Guilherme Moreno <guilhermemoreno200@gmail.com>
2026-03-17 20:26:05 -0300

    aplicando solid

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,11 @@
 import fastify from "fastify";
-import { PrismaClient } from "./generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { z } from 'zod'
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./http/routes";
 
 const app = fastify();
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-})
-const prisma = new PrismaClient({ adapter });
 
-prisma.user.create({
-    data: {
-        name: "Guilherme",
-        email: "guilherme@email.com"
-    }
-})
+app.register(appRoutes)
+
+
 export { app };
---
+++ src/use-cases/register.ts
@@ -0,0 +1,42 @@
+import { prisma } from "@/lib/prisma"
+import { hash } from "bcryptjs"
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository"
+
+interface RegisterUseCaseRequest {
+    name: string,
+    email: string,
+    password: string
+}
+
+export class RegisterUseCase {
+    private usersRepository: any
+    constructor(usersRepository: any) {
+        this.usersRepository = usersRepository
+    }
+
+    async execute({
+        name,
+        email,
+        password
+    }: RegisterUseCaseRequest) {
+        const password_hash = await hash(password, 6)
+
+        const userWithSameEmail = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        if (userWithSameEmail) {
+            throw new Error('Email already exists')
+        }
+
+        const prismaUsersRepository = new PrismaUsersRepository()
+        await prismaUsersRepository.create({
+            name,
+            email,
+            password_hash
+        })
+
+    }
+}
